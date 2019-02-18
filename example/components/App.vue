dc11deb043964c8c2a94b0faa0396d8bb1ae7e02
Pan Hezeng <panhezeng@gmail.com>
2019-02-18 11:35:07 +0800

    依赖模块版本优化

--- example/components/App.vue
+++ example/components/App.vue
@@ -1,42 +1,47 @@
 <template>
   <div id="app">
-    <h1>因为apiary的mock比较弱，如想获得预期效果，纯搜索只支持输入 Option name 1 ，创建功能只支持输入 Option name 2</h1>
+    <h1>
+      因为apiary的mock比较弱，如想获得预期效果，纯搜索只支持输入 Option name 1
+      ，创建功能只支持输入 Option name 2
+    </h1>
     <el-select-remote-multiple
       :api-url-options="apiUrlOptions"
       :api-url-create="apiUrlCreate"
       api-url-options-res-path="data.data"
-      :selected-obj.sync="selectedObj" :selected.sync="selected"
-      placeholder="输入关键字，从搜索结果中选择"></el-select-remote-multiple>
+      :selected-obj.sync="selectedObj"
+      :selected.sync="selected"
+      placeholder="输入关键字，从搜索结果中选择"
+    ></el-select-remote-multiple>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
+//  import ElSelectRemoteMultiple from '../../dist/el-select-remote-multiple.min'
+//  const ElSelectRemoteMultiple = require('../../dist/el-select-remote-multiple.min')
 
-  import Vue from 'vue'
-  //  import ElSelectRemoteMultiple from '../../dist/el-select-remote-multiple.min'
-  //  const ElSelectRemoteMultiple = require('../../dist/el-select-remote-multiple.min')
+//  Vue.use(ElSelectRemoteMultiple)
 
-  //  Vue.use(ElSelectRemoteMultiple)
+// import '@panhezeng/vue-axios'
 
-  import '@panhezeng/vue-axios'
-
-  window.ElSelectRemoteMultipleOptions = {ajax: Vue.http}
+window.ElSelectRemoteMultipleOptions = { ajax: Vue.http };
 //  require('../../dist/el-select-remote-multiple.min')
-    require('../../src')
+require("../../src");
 
-  //  const ElSelectRemoteMultiple = () => import('../../dist/el-select-remote-multiple.min')
+//  const ElSelectRemoteMultiple = () => import('../../dist/el-select-remote-multiple.min')
 
-  export default {
-    name: 'App',
-//    components: {ElSelectRemoteMultiple},
-    data () {
-      return {
-        apiUrlOptions: 'https://private-87040-publicexample.apiary-mock.com/el-select-remote-multiple/options',
-        apiUrlCreate: 'https://private-87040-publicexample.apiary-mock.com/el-select-remote-multiple/option',
-        selected: [],
-        selectedObj: []
-      }
-    }
+export default {
+  name: "App",
+  //    components: {ElSelectRemoteMultiple},
+  data() {
+    return {
+      apiUrlOptions:
+        "https://private-87040-publicexample.apiary-mock.com/el-select-remote-multiple/options",
+      apiUrlCreate:
+        "https://private-87040-publicexample.apiary-mock.com/el-select-remote-multiple/option",
+      selected: [],
+      selectedObj: []
+    };
   }
+};
 </script>
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,73 @@
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   output: {
-    filename: 'el-select-remote-multiple.min.js',
-    libraryTarget: 'umd',
+    filename: "el-select-remote-multiple.min.js",
+    libraryTarget: "umd",
     umdNamedDefine: true,
-    library: 'ElSelectRemoteMultiple',
-    libraryExport: 'ElSelectRemoteMultiple'
+    library: "ElSelectRemoteMultiple",
+    libraryExport: "ElSelectRemoteMultiple"
   },
   externals: {
-    '@panhezeng/utils/dist/get-object-item-by-path.js': '@panhezeng/utils/dist/get-object-item-by-path.js',
-    'vue': 'Vue',
-    'element-ui': {
-      commonjs: 'element-ui',
-      commonjs2: 'element-ui',
-      amd: 'element-ui',
-      root: 'ELEMENT'
+    "@panhezeng/utils/dist/get-object-item-by-path.js": {
+      commonjs: "@panhezeng/utils/dist/get-object-item-by-path.js",
+      commonjs2: "@panhezeng/utils/dist/get-object-item-by-path.js",
+      amd: "@panhezeng/utils/dist/get-object-item-by-path.js",
+      root: "getObjectItemByPath"
+    },
+    vue: {
+      commonjs: "vue",
+      commonjs2: "vue",
+      amd: "vue",
+      root: "Vue"
+    },
+    "element-ui": {
+      commonjs: "element-ui",
+      commonjs2: "element-ui",
+      amd: "element-ui",
+      root: "ELEMENT"
     }
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        use: 'vue-loader'
+        use: "vue-loader"
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: "babel-loader"
       },
       {
         test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ]
+        use: ["vue-style-loader", "css-loader"]
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'img/[name].[hash:7].[ext]'
+          name: "img/[name].[hash:7].[ext]"
         }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'media/[name].[hash:7].[ext]'
+          name: "media/[name].[hash:7].[ext]"
         }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
-          name: 'fonts/[name].[hash:7].[ext]'
+          name: "fonts/[name].[hash:7].[ext]"
         }
       }
     ]
   },
-  plugins: [
-    new VueLoaderPlugin()
-  ]
-}
+  plugins: [new VueLoaderPlugin()]
+};
